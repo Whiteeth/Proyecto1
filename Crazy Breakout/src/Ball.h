404f3c62edb24e8a2d84485b7aba526e744365e3
Joao <joao@joao-Satellite-L955>
2016-02-28 10:18:23 -0600

    Create ball header

---
+++ Crazy Breakout/src/Ball.h
@@ -0,0 +1,28 @@
+/*
+ * Ball.h
+ *
+ *  Created on: 28 de feb. de 2016
+ *      Author: joao
+ */
+ // Ball Header
+
+#ifndef BALL_H_
+#define BALL_H_
+
+class Ball{
+
+private:
+	int PosX;
+	int PosY;
+	int Size;
+	int Speed;
+	int Deap;
+
+	Ball(int PosX,int PosY,int inSize,int Speed,int Deap);
+};
+
+
+
+
+
+#endif /* BALL_H_ */
